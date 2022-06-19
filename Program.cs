33b410e5c706b11285e1a7db558430302753a0f9
doannhom1 <thotin2003@gmail.com>
2022-06-19 21:24:52 +0700

    list

---
+++ Program.cs
@@ -0,0 +1,51 @@
+﻿
+using System;
+using System.Collections;
+
+namespace CS  {
+     class Video {
+        public  string nameVideo {set;get;}
+        public  int stt {set;get;}
+        public double time {set;get;}
+        }
+
+        class PlayList  {
+        public string namePlaylist;
+    
+       
+        }
+
+    
+  
+    class program {
+        
+        
+        static void Main(string [] agrs){
+
+            List<Video> InfoVideo = new List<Video>(){
+                new Video (){
+                    nameVideo = "bai 1: co ban oop",
+                    stt = 1,
+                    time = 01.35
+                },
+                new Video (){
+                    nameVideo = "bai 2: co ban html",
+                    stt = 2,
+                    time = 01.35
+                },
+                new Video (){
+                    nameVideo = "bai 3: co ban oop 2",
+                    stt = 3,
+                    time = 01.35
+                }
+            } ;
+            
+            foreach(var p in InfoVideo)
+            {
+                Console.WriteLine($"video ({p.stt} ,{p.nameVideo} ,{p.time})");
+            }
+
+
+        }
+    }
+}
